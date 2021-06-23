4e653156f1015bb9bc7e383630c47fee3716e479
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2021-06-23 09:18:24 +0100

    Replace comment

--- ProjectCode/Data.java
+++ ProjectCode/Data.java
@@ -1,15 +1,13 @@
 /**
-* All material is for learning only.
-* All who use this material - whatever the
-* Purpose - must keep the reference to the original material, created by:
-* Helder Oliveira / Denis Botnaru 
-* and available at the address of your repository:
-* https://github.com/lireupt/ public.
-* This material may not be used in
-* No circumstance to replicate - integral or
-* partially - by authors/publishers to create books, and with
-* purposes of obtaining financial gains from it.
-*/
+   * All material is for learning only.
+   * All who use this material - whatever the purpose:
+   * - must keep the reference to the original material created by:
+   * - Helder Oliveira / Denis Botnaru
+   * and available at the address of your repository:
+   * - https://github.com/lireupt/ public.
+   * This material may not be used in no circumstance to replicate:
+   * - integral or partially, with purposes of obtaining financial gains from it.
+   */
 
 
 import java.awt.Point;
